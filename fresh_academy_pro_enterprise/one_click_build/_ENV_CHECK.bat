264fb2ffacb8269a0f092054a88f0b8a7edc376f
minasaleh2022 <105022361+minasaleh2022@users.noreply.github.com>
2025-11-04 22:59:11 +0200

    Delete fresh_academy_pro_enterprise/one_click_build directory

--- fresh_academy_pro_enterprise/one_click_build/_ENV_CHECK.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-where flutter
-if %ERRORLEVEL% NEQ 0 echo [!] Flutter not in PATH
-flutter --version
-flutter doctor -v
-echo.
-echo If there are red X errors above, share them here to get fixes.
-pause
